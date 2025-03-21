061674b513e15c2240583bbb451c7c233632500e
Kobe Shelby <Kobeshelby04@gmail.ccom>
2025-03-20 21:53:43 -0700

    Added logo

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import '../Style/Home.css';
+import logo from '../Images/Logo.png'
 function HomePage() {
     return (
         <>
             <section id='nav_sec'>
                 <nav className='navbar'>
+                    <img src={logo} className='logo-img' alt="Logo" />
                     <ul>
                         <a href=""><li>Clothing</li></a>
                         <a href=""><li>Electronics</li></a>
