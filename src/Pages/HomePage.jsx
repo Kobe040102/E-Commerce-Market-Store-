8e1d86460f9f112341a85486378ef43101d60fb7
Kobe Shelby <Kobeshelby04@gmail.ccom>
2025-03-21 22:07:57 -0700

    Finished navbar

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,21 +1,51 @@
 import '../Style/Home.css';
 import logo from '../Images/Logo.png'
+import cart from '../Images/cart.png'
+import profile from '../Images/user.png';
 function HomePage() {
     return (
         <>
             <section id='nav_sec'>
-                <div className='nav-sub'>
-                    <h1 className='title'><marquee scrollamount="15" direction="right">E-Commerce Website</marquee></h1>
-                </div>
                 <nav className='navbar'>
                     <div className='title-container'>
-                        <img src={logo} className='logo-img' alt="Logo" />
-                        <input type="text" className='bar' placeholder='Find..' />
+                        <ul className='nav-list'>
+                            <div className='title'>
+                                <h1>HexaCart</h1>
+                                <li><img src={logo} className='logo-img' alt="Logo" /></li>
+                            </div>
+                            <div className='catergories-container'>
+                                <ul className='catergories'>
+                                    <li>
+                                        <a href="">Clothing</a>
+                                    </li>
+                                    <li>
+                                        <a href="">Electronics</a>
+                                    </li>
+                                    <li>
+                                        <a href="">Accessories</a>
+                                    </li>
+                                    <li>
+                                        <a href="">HomeGoods</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className='right-items'>
+                                <li><input type="text" className='bar' placeholder='Search...' /></li>
+                                <li className='right-item'>
+                                    <img src={cart} className='icon' alt="icon" />
+                                    <a href=""><h3>Cart</h3></a>
+                                </li>
+                                <li className='right-item'>
+                                    <img src={profile} className='icon' alt="icon" />
+                                    <a href=""><h3>Profile</h3></a>
+                                </li>
+                            </div>
+                        </ul>
                     </div>
                 </nav>
             </section>
             <section id='main-sec'>
-
+            
 
             </section>
         </>
