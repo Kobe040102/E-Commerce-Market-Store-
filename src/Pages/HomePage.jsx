af7fc81c4d4ca38636594c7997cf1263a9b33f70
Kobe Shelby <Kobeshelby04@gmail.ccom>
2025-03-28 11:05:50 -0700

    New Electronics section

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -43,28 +43,66 @@ function HomePage() {
                     </ul>
                 </div>
                 <div className='Clothing-container'>
-                    <h1 className='clothing-title'>Check Our Clothing</h1>
+                    <h1 className='best-title'>Check Our Clothing</h1>
                     <div className='scoll-container'>
                         <Scroller />
                         <a href="/ClothingPage" className='clicking'><h1 className='click-info'>Click for more</h1></a>
                     </div>
                 </div>
+                <div className='new-release-container'>
+                    <h1 className='best-title'>
+                        New Electronics
+                    </h1>
+                </div>
             </section>
             <section id="footer">
                 <div className='footer-container'>
                     <ul className='footer-links'>
-                        <li><h3>Contact us</h3></li>
-                        <li><h3>Acount Services</h3></li>
-                        <li><h3>Shopping Guide</h3></li>
+                        <li>
+                            <h3 className='social-title'>Contact us</h3>
+                            <div className='icon1'>
+                            <p>(888)-xxx-2345</p>
+                            </div>
+                            <div className='icon1'>
+                            <p>support@hexacart.com</p>
+                            </div>
+                            <div className='icon1'>
+                            <p>Mon - Fri, 9AM - 5 AM PST</p>
+                            </div>
+                        </li>
+                        <li>
+                            <h3 className='social-title'>Account Services</h3>
+                            <div className='icon1'>
+                                <p>Sign In/Register</p>
+                            </div>
+                            <div className='icon1'>
+                            <p>Order History</p>
+                            </div>
+                            <div className='icon1'>
+                            <p>Saved Items / Wishlist</p>
+                            </div>
+                        </li>
+                        <li>
+                            <h3 className='social-title'>Shopping Guide</h3>
+                            <div className='icon1'>
+                            <p>Payment & Checkout</p>
+                            </div>
+                            <div className='icon1'>
+                            <p>Returns & Refunds</p>
+                            </div>
+                            <div className='icon1'>
+                            <p>FAQs</p>
+                            </div>
+                        </li>
                         <li>
                             <h3 className='social-title'>Social Media</h3>
                             <div className='icon1'>
                                 <p>Instagram</p><img src={insta} className='social-icon' alt="insta" />
                             </div>
-                            <div className='icon2'>
+                            <div className='icon1'>
                                 <p>Facebook</p> <img src={facebook} className='social-icon' alt="facebook" />
                             </div>
-                            <div className='icon3'>
+                            <div className='icon1'>
                                 <p>X</p> <img src={twitter} className='social-icon' alt="twitter" />
                             </div>
                         </li>
