f9a42e975f6c6a8eea2040b6a3a98e2921fc681e
Kobe Shelby <Kobeshelby04@gmail.ccom>
2025-04-01 20:25:57 -0700

    Adding stylesheets to each page

--- src/Pages/ClothingPage.jsx
+++ src/Pages/ClothingPage.jsx
@@ -6,8 +6,20 @@ function Clothes() {
         <>
         <Navbar />
             <section id="main">
-                <div>
-                    <h1 className='Clothing'>Clothing</h1>
+                <div className='Clothing-container'>
+                    <h1 className='Clothing-title'>Clothing</h1>
+                    <div className='Shirt-container'>
+                        <h1 className='proc-title'>Shirts</h1>
+                    </div>
+                    <div className='Pants-container'>
+                    <h1 className='proc-title'>Pants</h1>
+                    </div>
+                    <div className='Jacket-container'>
+                    <h1 className='proc-title'>Jackets</h1>
+                    </div>
+                    <div className='Shoe-container'>
+                    <h1 className='proc-title'>Shoes</h1>
+                    </div>
                 </div>
             </section>
             <Footer />
