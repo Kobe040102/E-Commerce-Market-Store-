acc526228fed7780525220f8276195a36555c340
Kobe Shelby <Kobeshelby04@gmail.ccom>
2025-06-10 14:37:42 -0700

    Starting Accessories page

--- src/Pages/AccessoriesPage.jsx
+++ src/Pages/AccessoriesPage.jsx
@@ -1,5 +1,7 @@
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
+import Hat1 from '../Images/Accessories/Hat1.jpg';
+import '../Style/AccessoriesStyle.css';
 function Accessories() {
     return(
         <>
@@ -9,6 +11,11 @@ function Accessories() {
                     <h1 className='Clothing-title'> Fashion Accessories</h1>
                     <div className='Shirt-container'>
                         <h1 className='proc-title'>Hats</h1>
+                        <ul>
+                            <li>
+                                <img src= {Hat1} className='product-img' alt="" />
+                            </li>
+                        </ul>
                     </div>
                     <div className='Pants-container'>
                     <h1 className='proc-title'>Sun Glasses</h1>
